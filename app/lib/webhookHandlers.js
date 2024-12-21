3308094f558c6f8786178a78a0234c2778916a1a
moneer95 <mnyrskyk@gmail.com>
2024-12-21 11:43:13 +0300

    decrease

--- app/lib/webhookHandlers.js
+++ app/lib/webhookHandlers.js
@@ -7,6 +7,8 @@ function divideItems(itemsMetadata) {
         // console.log(`Item: ${item.description}`);
         console.log(`Metadata:`, metadata);
 
+        console.log(metadata.doctype)
+
         switch(metadata.doctype){
             case "Products":
                 handleProductPurchase(metadata)
